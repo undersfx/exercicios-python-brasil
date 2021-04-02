9d6ae80b913287c6ea642c0b9b816a07e28e6dfd
undersfx <atchin_@hotmail.com>
2021-04-02 20:41:52 -0300

    add: Exercícios 1-4

---
+++ lista-1/4.py
@@ -0,0 +1,8 @@
+nota1 = int(input('Digite uma nota: '))
+nota2 = int(input('Digite uma nota: '))
+nota3 = int(input('Digite uma nota: '))
+nota4 = int(input('Digite uma nota: '))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f"O aluno teve uma média de {media} pontos.")
